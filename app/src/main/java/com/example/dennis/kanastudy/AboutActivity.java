aa27a2d1d54ac068abf09d9e5f09ae7ac7ba2473
DennisPacewicz <dennis.pacewicz@gmail.com>
2015-04-13 20:54:38 -0400

    created github web link

--- app/src/main/java/com/example/dennis/kanastudy/AboutActivity.java
+++ app/src/main/java/com/example/dennis/kanastudy/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.example.dennis.kanastudy;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by dennis on 13/04/15.
@@ -14,5 +17,13 @@ public class AboutActivity extends Activity{
         setContentView(R.layout.about_layout);
 
 
+    }
+    public void goToGit (View view) {
+        goToUrl ( "https://github.com/DennisPacewicz/");
+    }
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
